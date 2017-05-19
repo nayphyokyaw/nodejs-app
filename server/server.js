04375458060e780eb05fed45ea0fc93420fd91c3
nayphyokyaw <nayphyokyaw@gmail.com>
2017-05-19 19:56:37 +0630

    create generateMessate utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const prots = process.env.PORT || 3000
 var app = express();
@@ -10,50 +11,22 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
 io.on('connection', (socket) => {  //index.html socket
   console.log('new user connected');
 
-  // socket.emit('newMessage', {
-  //   from: 'Jhon',
-  //   text: 'someOne call me.'
-  // });
+  socket.emit('newMessage', generateMessage('Admin','welcome to chat app'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hay. What is going on',
-  //   createAt: 212
-  //   });
-
- // socket.emit from Admin text welcome to the chat app
- socket.emit('newMessage',{
-   from: 'Admin',
-   text: 'welcome to the chat app',
-   createAt: new Date().getTime()
- });
- // socket.broadcast.emit from Admin text New user joined
- socket.broadcast.emit('newMessage', {
-   from: 'Admin',
-   text: 'New user Joined',
-   createAt: new Date().getTime()
- });
-    socket.on('createMessage',(message) => {
-      console.log('createMessage', message);
-      io.emit('newMessage', {
-        form: message.from,
-        text: message.text,
-        createAt: new Date().getTime() // Time method
-      });
-
-    // socket.broadcast.emit('newMessage',{
-    //   from: message.form,
-    //   text: message.text,
-    //   createAt: new Date().getTime()
-    // });
-    });
-
-    socket.on('createEmail', (newEmail) => {
-      console.log('createEmail', newEmail);
-    });
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+     // socket.broadcast.emit('newMessage', {
+     //   from: message.from,
+     //   text: message.text,
+     //   createdAt: new Date().getTime()
+     // });
+});
 
   socket.on('disconnect', () => {
     console.log('User was disconnect');
@@ -62,4 +35,4 @@ io.on('connection', (socket) => {  //index.html socket
 
 server.listen(3000, () => {
   console.log(`Server is starting on prots: ${prots}`);
-})
+});
