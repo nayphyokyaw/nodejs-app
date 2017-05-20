1d07193dd1ab15391d2b9708baf5b7b611b4f350
nayphyokyaw <nayphyokyaw@gmail.com>
2017-05-20 16:29:09 +0630

    Add Form for messages and show incoming in browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,24 +2,30 @@ var socket = io('http://localhost:3000');
 
 socket.on('connect', function(){
   console.log('connected to server');
-
-  // socket.emit('createMessage',{
-  //     from: 'Andrew',
-  //     text: 'Yup, That works for me.'
-  //   });
-
-  // socket.emit('createEmail',{
-  //   to: 'Jen@example.com',
-  //   text: 'Hey.This is Andrew'
-  // });
 });
 socket.on('disconnect', function(){
   console.log('Disconnect to server');
 });
-
 socket.on('newMessage', function(message){
   console.log('newMessage',message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
-// socket.on('newEmail', function(email){
-//   console.log('New email', email);
-// });
+//Evernt ACKnoledgements
+// socket.emit('createMessage', {
+//     from:'Frank',
+//     text:'Hi'
+// }, function (data) {
+//     console.log('Got it', data);
+// });// alart message server and clients
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function(){
+    });
+});
--- server/server.js
+++ server/server.js
@@ -10,28 +10,31 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // path join
 
 io.on('connection', (socket) => {  //index.html socket
   console.log('new user connected');
-
+//admin and all clients show newMessage when start web chats app
   socket.emit('newMessage', generateMessage('Admin','welcome to chat app'));
+//admin alway show clients newMessage when start web chats app
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the Server');
      // socket.broadcast.emit('newMessage', {
      //   from: message.from,
      //   text: message.text,
      //   createdAt: new Date().getTime()
      // });
-});
+});//socket.on clients & server createMessage ending
 
   socket.on('disconnect', () => {
     console.log('User was disconnect');
-  });
-});
+});// socket.on server message disconnect ending
+});//io.on server message connection ending
 
 server.listen(3000, () => {
   console.log(`Server is starting on prots: ${prots}`);
